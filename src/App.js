f38565d1632f4675ba3355aa130892e04bea452c
RaquelDommingues <110355736+RaquelDommingues@users.noreply.github.com>
2023-06-18 14:58:03 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}
+        <a 
+          href='https://github.com/RaquelDommingues/react-weather-app'
+          target='_blank'
+        >Raquel Domingues
+        </a>
+      </footer>
     </div>
   );
 }
