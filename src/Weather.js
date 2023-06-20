5d3e088a6bcc9c195655850c3fdd5db8ebed5f6e
RaquelDommingues <110355736+RaquelDommingues@users.noreply.github.com>
2023-06-20 16:38:13 +0100

    API Integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 import { FaSearch } from 'react-icons/fa';
 import sun from './img/sun.png';
 
 export default function Weather() {
-    return(
-        <div className="Weather">
-            <div className="Query">
-                <form id="searchBar">
-                    <input
-                        type="search"
-                        placeholder="Location"
-                        autocomplete="off"
-                        class="form-control"
-                        id="city-input"
-                        />
-                    <button className="glass-button-icon" type="submit">
-                        <FaSearch/>
-                    </button>
-                </form>
-            </div>
-            <div>
-                    <div className="row ">
+    const[weatherData, setWeatherData] = useState({ready: false});
+    function handleResponse (response) {
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.temperature.current,
+            date: "Wednesday 07:00",
+            wind: response.data.wind.speed,
+            humidity: response.data.temperature.humidity,
+            condition: response.data.condition.description,
+            city: response.data.city
+        }
+        );
+    }
+
+    if(weatherData.ready){
+        return(
+            <div className="Weather">
+                <div className="Query">
+                    <form id="searchBar">
+                        <input
+                            type="search"
+                            placeholder="Location"
+                            autocomplete="off"
+                            class="form-control"
+                            id="city-input"
+                            />
+                        <button className="glass-button-icon" type="submit">
+                            <FaSearch/>
+                        </button>
+                    </form>
+                </div>
+                <div>
+                    <div className="row">
                         <div className="col-md-6">
                             <ul>
-                                <li className="location-time"><h1>Lisbon</h1></li>
-                                <li className="location-time">Wednesday 07:00</li>
+                                <li className="location-time"><h1>{weatherData.city}</h1></li>
+                                <li className="location-time">{weatherData.date}</li>
                                 <br />
-                                <li className="weather-conditions">Precipitation: 15%</li>
-                                <li className="weather-conditions">Humidity: 5%</li>
-                                <li className="weather-conditions">Wind: 12 km/h</li>
+                                <li className="weather-conditions text-capitalize">{weatherData.condition}</li>
+                                <li className="weather-conditions">Humidity: {weatherData.humidity}%</li>
+                                <li className="weather-conditions">Wind: {Math.round(weatherData.wind)} km/h</li>
                             </ul>
                         </div>
-                        <div className="col-6">
-                            <div className="row">
-                                <div className="col-12 mb-3">
-                                    <img className="weather-icon" src={sun} alt="weather-icon"/>
+                        <div className="col-md-6">
+                                <div className="row">
+                                    <div className="col-12 mb-6">
+                                        <img className="weather-icon" src={sun} alt="weather-icon"/>
+                                    </div>
+                                    <div className="col-6 c-temp">
+                                        <div className="current-temp">{Math.round(weatherData.temperature)}<sup className="current-units">ºC</sup></div>
+                                    </div>
+                                    <div className="col-6 mb-6">
+                                        <div className="max-temp">30<sup className="units">ºC</sup></div>
+                                        <div className="min-temp">10<sup className="units">ºC</sup></div>
+                                    </div>
                                 </div>
-                                <div className="col-6 c-temp">
-                                <div className="current-temp">20ºC</div>
-                                </div>
-                                <div className="col-6 mb-3">
-                                    <div className="max-temp">30ºC</div>
-                                    <div className="min-temp">10ºC</div>
-                                </div>
-                            </div>
-                        </div> 
+                        </div>
                     </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+    const apiKey = "1059t88f30693a8921307c53o34fadfb";
+    let city = "London"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+    }
 }
